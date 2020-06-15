aee7b517d2f0e84dd1e3e06499cc9daee3c4ffd6
Jeff Browning <browningjeff1@gmail.com>
2020-06-15 09:56:08 -0500

    Created backend

--- backend/server.js
+++ backend/server.js
@@ -11,11 +11,17 @@ app.use(cors());
 app.use(express.json());
 
 const uri = process.env.ATLAS_URI;
-mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true });
-const connnection = mongoose.connection;
+mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true });
+const connection = mongoose.connection;
 connection.once('open', () => {
     console.log('MongoDB database connection established succesfully');
 })
+
+const exercisesRouter = require('./routes/exercises');
+const userRouter = require('./routes/users');
+
+app.use('/exercises', exercisesRouter);
+app.use('/users', userRouter);
 
 
 app.listen(port, () => {
