44c527b3a7ff6008d76920ec800a480d99e585bf
Jeff Browning <browningjeff1@gmail.com>
2020-06-15 10:51:34 -0500

    Created Component Templates

---
+++ src/components/create-exercise.component.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class CreateExercise extends Component {
+    render() {
+        return (
+            <div>
+                <p>You are a create exercise component!</p>
+            </div>
+        )
+    }
+}
